2099948b266746e00307ef2ab5513fe6e58f4cd2
sethukumar0001 <sethukumar0001@gmail.com>
2023-05-04 22:55:53 +0530

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { PostsModule } from './components/posts/post.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [PostsModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationOptions: {
+        allowUnknown: true,
+        abortEarly: true,
+      },
+    }),
+    DatabaseModule,
+    PostsModule,
+  ],
 })
 export class AppModule {}
--- src/components/posts/post.module.ts
+++ src/components/posts/post.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Post } from './entities/post.entity';
 import { PostsController } from './post.controller';
 import { PostsService } from './post.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Post])],
   controllers: [PostsController],
   providers: [PostsService],
   exports: [PostsService],
